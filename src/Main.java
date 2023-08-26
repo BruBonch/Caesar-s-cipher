5cf3598cda809a53afc1d9578d66634493a384e1
Ilya Emelyanov <baralgin38@yandex.ru>
2023-08-26 17:34:18 +0300

    Calls encryption and decryption methods

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 package src;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        String ALPHABET_STRING = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,\":-!? ";
+        String ALPHABET_STRING = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя.,\":-!? \n";
         int ENCRYPT_KEY = 3;
 
         Encryptor encryptor = new Encryptor();
+        Decryptor decryptor = new Decryptor();
 
         try {
-            encryptor.encrypt("D:/file.txt", ALPHABET_STRING, ENCRYPT_KEY);
+            encryptor.encrypt("D:/file_1.txt", ALPHABET_STRING, ENCRYPT_KEY);
+            decryptor.decrypt("D:/encrypted.txt", ALPHABET_STRING, ENCRYPT_KEY);
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
